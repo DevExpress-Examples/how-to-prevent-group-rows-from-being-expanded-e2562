8710efbbbd9c08357f29d0236badc5705c030ef9
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Prevent Group Rows from Expanding/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using DevExpress.Xpf.Grid;
+using DXExample.DemoData;
+
+namespace Prevent_Group_Rows_from_Expanding {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            gridControl1.ItemsSource = Invoice.GetData();
+        }
+        private void gridControl1_GroupRowExpanding(object sender, RowAllowEventArgs e) {
+            if(e.Row == null ||
+                ((Invoice)e.Row).Status == InvoiceStatus.Invalidated &&
+                gridControl1.Columns["Status"].GroupIndex != -1)
+                e.Allow = false;
+        }
+    }
+}
---
+++ VB/Prevent Group Rows from Expanding/MainPage.xaml.vb
@@ -0,0 +1,19 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows.Controls
+Imports DevExpress.Xpf.Grid
+Imports DXExample.DemoData
+
+Namespace Prevent_Group_Rows_from_Expanding
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			gridControl1.ItemsSource = Invoice.GetData()
+		End Sub
+		Private Sub gridControl1_GroupRowExpanding(ByVal sender As Object, ByVal e As RowAllowEventArgs)
+			If e.Row Is Nothing OrElse (CType(e.Row, Invoice)).Status = InvoiceStatus.Invalidated AndAlso gridControl1.Columns("Status").GroupIndex <> -1 Then
+				e.Allow = False
+			End If
+		End Sub
+	End Class
+End Namespace
